3a336db25d2a11d78e239405257a1d37e7c8e8c6
krisito2000 <83059971+krisito2000@users.noreply.github.com>
2024-01-17 22:15:20 +0200

    Circle Expand Animation and Tutorial Level Part 2 Initiation
    - Integrated a circle expand animation upon hovering over the circle to impart a button-like effect.
    - Made minor adjustments to the button animation for improved visual consistency.
    - Initiated Part 2 of the Tutorial level

--- Assets/Scripts/Animations/MainMenuCircleTransition.cs
+++ Assets/Scripts/Animations/MainMenuCircleTransition.cs
@@ -5,8 +5,29 @@ using UnityEngine;
 public class MainMenuCircleTransition : MonoBehaviour
 {
     public Animator animator;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            CircleTransition();
+        }
+    }
     public void CircleTransition()
     {
-        animator.SetBool("isTriggered", true);
+        if (!animator.GetBool("isExpanded"))
+        {
+            animator.SetTrigger("Clicked");
+            animator.SetBool("isExpanded", true);
+        }
+    }
+    public void OnHover()
+    {
+        animator.SetTrigger("Hover");
+    }
+
+    public void OnUnHover()
+    {
+        animator.SetTrigger("Normal");
     }
 }
